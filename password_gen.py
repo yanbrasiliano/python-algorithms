3eb37d2d0b8a6a2a24efeeb8b60785813b48d7c2
yanbrasiliano <yanpenabr@gmail.com>
2021-01-27 15:08:20 -0300

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -1,7 +1,27 @@
-import random
-import secrets
-l = ['!','#','%','&']
-x = random.randint(0,15)
-y = random.choice(l)
-for c in range (0,x):
-	print(f'{x}{y}',end='')
+
+#Este script serve para gerar senhas de 10 dígitos de forma automática e aleatória.
+
+from random import choice
+import string
+
+'''string.ascii_lowercase # abcdefghijklmnopqrstuvwxyz
+string.ascii_uppercase # ABCDEFGHIJKLMNOPQRSTUVWXYZ
+string.ascii_letters # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+string.digits # 0123456789
+string.punctuation # <=>?@[\]^_`{|}~'''
+
+size = int(input('Enter the password size: '))
+
+while size <= 5:
+	print('Easy password to break, recommended: 8 or more characters.')
+	size = int(input('Enter the password size: '))
+
+v= string.ascii_letters + string.digits + string.punctuation 
+
+
+password =' '
+
+for i in range (size):
+	password+=choice(v)
+print()
+print(f'Password generated: {password}')
