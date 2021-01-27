c45eba0f8d54960d467ee074520b20eb3cec593c
yanbrasiliano <yanpenabr@gmail.com>
2021-01-27 16:49:57 -0300

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -3,25 +3,107 @@
 
 from random import choice
 import string
+from time import sleep
+import os
 
 '''string.ascii_lowercase # abcdefghijklmnopqrstuvwxyz
 string.ascii_uppercase # ABCDEFGHIJKLMNOPQRSTUVWXYZ
 string.ascii_letters # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 string.digits # 0123456789
 string.punctuation # <=>?@[\]^_`{|}~'''
+while True:
+	print()
+	print('\t	GENERATOR STARS')
+	print('-'*60)
+	print('''	[ 1 ] Password with all characters.\n
+	[ 2 ]  Password with letters and numbers.\n
+	[ 3 ]  Password only with letters[UPPER and LOWER].\n
+	[ 4 ]  Password only with numbers.''')
+	print('-'*60)
 
-size = int(input('Enter the password size: '))
-
-while size <= 5:
-	print('Easy password to break, recommended: 8 or more characters.')
+	choose = int(input('Option: '))
 	size = int(input('Enter the password size: '))
+	while size <= 5:
+		print('Easy password to break, recommended: 6 or more characters.')
+		size = int(input('Enter the password size: '))
+	if choose == 1:
+		v= string.ascii_letters + string.digits + string.punctuation
+		password =' '
+		for i in range (size):
+			password+=choice(v)
+		os.system('clear')
+		print('Generating password...')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print()
+		print(f'Password generated:{password}')
+		print()
+	if choose == 2: 
+		v= string.ascii_letters + string.digits
+		password =' '
+		for i in range (size):
+			password+=choice(v)
+		os.system('clear')
+		print('Generating password...')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print()
+		print(f'Password generated:{password}')
+		print()
+	if choose == 3:
+		v= string.ascii_letters 
+		password =' '
 
-v= string.ascii_letters + string.digits + string.punctuation 
-
-
-password =' '
-
-for i in range (size):
-	password+=choice(v)
+		for i in range (size):
+			password+=choice(v)
+		os.system('clear')
+		print('Generating password...')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print()
+		print(f'Password generated:{password}')
+		print()
+	if choose == 4:
+		v= string.digits
+		password =' '
+		for i in range (size):
+			password+=choice(v)
+		os.system('clear')
+		print('Generating password...')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print('.')
+		sleep(1)
+		print()
+		print(f'Password generated:{password}')
+		print()
+	while True:
+		resp = str(input('GENERATE MORE PASSWORDS: [Y/N]? ')).upper().split()[0]
+		if resp in 'YN':
+				break
+		print('ERROR! ANSWER ONLY Y or N!')
+	if resp == 'N':
+			break
+os.system('clear')
+print('Thanks for using GENERATOR STARS.')
 print()
-print(f'Password generated: {password}')
+
+
