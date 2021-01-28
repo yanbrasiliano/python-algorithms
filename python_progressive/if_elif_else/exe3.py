43ff17a509888a6284181975dd38113817be3415
yanbrasiliano <yanpenabr@gmail.com>
2021-01-28 09:04:52 -0300

    Update exe3.py

--- python_progressive/if_elif_else/exe3.py
+++ python_progressive/if_elif_else/exe3.py
@@ -1,8 +1,20 @@
 ##APOSTILA PYTHON PROGRESSIVO
 
 # NÚMERO POR EXTENSO
+from time import sleep
 
 n = int(input('Number: '))
 
-u = n % 10
-c = n % 100
+u = n // 1 % 10
+d = n // 10 % 10
+c = n // 100 % 10
+m = n // 1000  % 10
+
+print(f'Analyzing number {n}...')
+print('.')
+sleep(1)
+print('.')
+sleep(1)
+print('.')
+sleep(1)
+print(f'M:{m}\nC:{c}\nD:{d}\nU:{u}')
