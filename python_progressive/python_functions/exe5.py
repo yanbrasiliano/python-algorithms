dc4820310b4beb9e60d07dac75db7a56a38b5952
yanbrasiliano <yanpenabr@gmail.com>
2021-02-05 10:14:12 -0300

    Update exe5.py

--- python_progressive/python_functions/exe5.py
+++ python_progressive/python_functions/exe5.py
@@ -1,2 +1,18 @@
 #Calcular o perimetro e a área do círculo com uma variável global.
 
+pi = 3.14
+
+def perimeter(radius):
+	p = 2 * pi * radius
+	print(f'Perimeter: {p}')
+
+def area(radius):
+	a = pi * (radius ** 2)
+	print(f'Area: {a}')
+
+
+
+
+r = float(input('Type the radius of the circumference: '))
+perimeter(r)
+area(r)
