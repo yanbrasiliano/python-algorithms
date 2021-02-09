40ca45d940244a7d1cf7296790a79aabe34f4a3f
yanbrasiliano <yanpenabr@gmail.com>
2021-02-08 23:11:14 -0300

    .

---
+++ python_progressive/python_functions/exe12.py
@@ -0,0 +1,20 @@
+#6. Crie uma função que recebe um inteiro positivo e teste para saber se ele é primo ou não. Faça um script que recebe um inteiro n e mostra todos os primos, de 1 até n.
+
+
+def prime(n):
+	while True:
+		for v in range(2,n):
+			if n % 2 == 0 and n != 2:
+				return False
+		return True
+
+def show():
+	n = int(input('Show range to value: '))
+	for v in range(2,n+1):
+		if (prime(v)):
+			print(v)
+
+
+
+while True:
+	show()
