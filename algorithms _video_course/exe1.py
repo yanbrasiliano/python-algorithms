407547bb94fac99e8fff313e8d5e80b30ad7edbb
yanbrasiliano <yanpenabr@gmail.com>
2021-02-09 23:50:42 -0300

    Update exe1.py

--- algorithms _video_course/exe1.py
+++ algorithms _video_course/exe1.py
@@ -1,7 +1,7 @@
 #98) Crie um programa que tenha uma função SuperSomador(), que vai receber dois números como parâmetro e depois vai retornar a soma de todos os valores no intervalo entre os valores recebidos. 
 
 def gap(x,y):
-	
+	s = 0 
 
 
 
