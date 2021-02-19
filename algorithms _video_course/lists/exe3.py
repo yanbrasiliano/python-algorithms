53555bff94d0dcb51483bf3b9407bbdc0ff050c5
yanbrasiliano <yanpenabr@gmail.com>
2021-02-19 10:39:00 -0300

    add exe

---
+++ algorithms _video_course/lists/exe3.py
@@ -0,0 +1,1 @@
+#LISTS - Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.	
