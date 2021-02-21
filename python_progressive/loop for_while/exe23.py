1595543b0d259e2cb5eb1aa447ff823c014f44ee
yanbrasiliano <yanpenabr@gmail.com>
2021-02-21 11:56:31 -0300

    Update exe23.py

--- python_progressive/loop for_while/exe23.py
+++ python_progressive/loop for_while/exe23.py
@@ -3,12 +3,21 @@ número inteiro fornecido pelo usuário.
 2. O programa deverá mostrar também o número de divisões que ele executou para encontrar os números primos;serão avaliados o funcionamento, o estilo e o número de testes (divisões)
 executados."""
 
-n = int(input('Number: '))
 
-prime = []
-for v in range(1,n+1):
-	print(f'{v}')
-	if n % v == 0:
-		prime.append(v)
+n = int(input('Enter a value: '))
+t = 0
+for c in range(1, n+1):
+    if n % c == 0:
+      t += 1
+    else:
+        print(f'\033[35m',end='')
+    print(f'{c} ', end=' ')
+print(f'\n\033[33m{n} divisible {t} times.\033[33m')
 
-print(prime)
+if t == 2:
+    print('\nIt is prime')
+else:
+    print('\nDo not prime.')
+
+
+
