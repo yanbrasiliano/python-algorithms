cd27ac3c4e77e65d9c797df261e74b7d2cdbbc21
yanbrasiliano <yanpenabr@gmail.com>
2021-02-25 01:04:29 -0300

    Update exe36.py

--- python_progressive/loop for_while/exe36.py
+++ python_progressive/loop for_while/exe36.py
@@ -2,3 +2,9 @@
 inteiro que será digitado pelo usuário, mas a tabuada não deve
 necessariamente iniciar em 1 e terminar em 10, o valor inicial e final devem
 ser informados também pelo usuário.'''
+
+first_num = int(input('Enter a first number in the range: '))
+last_num =int(input('Enter a first number in the range: ')) 
+
+for v in range(first_num,last_num+1,1):
+	
