22fe2b7b1fc4d2965770e06b1b2012116b98072c
yanbrasiliano <yanpenabr@gmail.com>
2021-02-26 10:28:15 -0300

    Create exe15.py

---
+++ python_progressive/python_functions/exe15.py
@@ -0,0 +1,13 @@
+"""
+4. A probabilidade de dar um valor em um dado é 1/6 (uma em 6). Faça um
+script em Python que simule 1 milhão de lançamentos de dados e mostre a
+frequência que deu para cada número.
+"""	
+
+from random import randint
+
+while True:
+	date = randint(1,6)
+	
+
+print(date)
