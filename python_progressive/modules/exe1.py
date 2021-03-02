8204963ad4c2bf2b236066a029ee1320b4e14ef5
yanbrasiliano <yanpenabr@gmail.com>
2021-03-02 19:54:55 -0300

    add exe

--- python_progressive/modules/exe1.py
+++ python_progressive/modules/exe1.py
@@ -3,3 +3,14 @@ Crie um script em Python que gere 6 números aleatórios para a Mega-Sena
 (neste jogo, você pode escolher dezenas de 1 até 60). Não pode repetir
 dezena.
 """
+
+from random import randint
+
+sort = 0 
+x = randint(1,60)
+for i in range(1,7):
+	sort = randint(1,60)
+	if sort != x:
+		x = sort
+
+	print(f'{i}° == {x}')
---
+++ python_progressive/modules/exe2.py
@@ -0,0 +1,8 @@
+"""
+Crie um jogo em Python onde o computador vai sortear um número de 1 até
+100.
+Em seguida, você vai tentar adivinhar que número foi esse.
+A cada tentativa, ele vai te dizer se seu palpite foi alto, baixo ou se você
+acertou.
+Quando acertar, deve mostrar quantas tentativas você fez até acertar.
+"""
