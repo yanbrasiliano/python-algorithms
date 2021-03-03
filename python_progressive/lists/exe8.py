b8665c956bd91eaa77ef1dccba34032a4f1965eb
yanbrasiliano <yanpenabr@gmail.com>
2021-03-03 10:07:27 -0300

    Update exe8.py

--- python_progressive/lists/exe8.py
+++ python_progressive/lists/exe8.py
@@ -3,4 +3,17 @@ Crie uma matriz 4 x 4 (ou seja, 4 linhas e 4 colunas, ou seja, 4 listas dentro
 de uma lista maior).
 Enxerte essa lista com números de 0, 1, 2, 3 etc
 '''
+array = [[0,0,0],[0,0,0],[0,0,0]]
 
+for l in range(0,3):  ## for para alimentar a matriz
+	for c in range(0,3):
+
+		array[l][c] = int(input(f'Enter a value for [{l}] [{c}]: '))
+
+print()
+print('-'*15)
+for l in range(0,3):  ## for para mostrar a matriz
+	for c in range(0,3):
+		print(f'[{array[l][c]:4^}]',end='')
+	print()
+print('-'*15)	
