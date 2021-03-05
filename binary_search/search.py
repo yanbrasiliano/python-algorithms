e4bdbe95a630bda00cd733120b23806a708e7a4e
yanbrasiliano <yanpenabr@gmail.com>
2021-03-05 11:00:33 -0300

    Update search.py

---
+++ binary_search/search.py
@@ -0,0 +1,14 @@
+from random import randint, random
+def binary_search(list,item):
+	smaller = 0 
+	biggest
+
+
+
+machine = [random(1,50)]
+
+choices = 0
+
+while user != cities: 
+	user = int(input('Your bet: '))
+	choices+=1
