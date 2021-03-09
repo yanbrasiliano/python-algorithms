695b47e95cd4d1ec41254377f95fe05c2ca34561
yanbrasiliano <yanpenabr@gmail.com>
2021-03-08 21:04:56 -0300

    Create school-register.py

---
+++ school-register.py
@@ -0,0 +1,21 @@
+"""
+	File name:	Register School                  
+	Author:	Yan Brasiliano S.Penalva	                       
+	Email: yanpenabr@gmail.com
+	---------------------------------------------------------------------
+	Date created: 08 MAR 21                    
+	Date last modified: 08 MAR 21
+	Python Version: 3.7.3
+	License: GPL
+	Maintainer: Yan Brasiliano S.Penalva
+	Version: 1.0
+	---------------------------------------------------------------------
+
+	Description: 
+	PT-BR: Um script para coordenar dados de uma turma, verificando notas,alunos,adicionar ou remover aluno da turma,média da turma, alteração de dados e inserção.
+	O script é uma base para meus estudos.
+	
+	EN-US: A script to manage class data, checking grades, students, adding or removing students from the class, class average, data change and insertion. 
+	The script is a basis for my studies. 
+
+"""
