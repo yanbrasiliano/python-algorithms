6afb1e2074c0783d55d603b027e34d029240af0c
yanbrasiliano <yanpenabr@gmail.com>
2021-03-17 10:39:22 -0300

    Update main.py

---  Python_Pro_Bootcamp_2021/day1/main.py
+++  Python_Pro_Bootcamp_2021/day1/main.py
@@ -1,6 +1,9 @@
 	"""
 
 	Exercise Day 1: Generator Name Band.
+	Details: A program to generate band names based on the information received.
+	Author: Yan Brasiliano SIlva Penalva <yanpenabr@gmail.com>
+	Date: 17 MAR 2021
 	Course:  Python Pro Bootcamp for 2021
 	Level: Beginner
 
