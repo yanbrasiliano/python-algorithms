f653454d83f49e3371220e5cdaeccc0de120d452
yanbrasiliano <yanpenabr@gmail.com>
2021-03-18 12:03:14 -0300

    add exe

---  python_pro_bootcamp_2021/day1/main.py
+++  python_pro_bootcamp_2021/day1/main.py
@@ -1,14 +1,17 @@
-	"""
+"""
 
-	Exercise Day 1: Generator Name Band.
-	Details: A program to generate band names based on the information received.
-	Author: Yan Brasiliano SIlva Penalva <yanpenabr@gmail.com>
-	Date: 17 MAR 2021
-	Course:  Python Pro Bootcamp for 2021
-	Level: Beginner
+    Challenge Day 1: Generator Name Band.
+    Details: A program to generate band names based on the information received.
+    Author: Yan Brasiliano SIlva Penalva <yanpenabr@gmail.com>
+    Date: 17 MAR 2021
+    Course:  Python Pro Bootcamp for 2021
+    Level: Beginner
 
-	"""
+"""
+
+print('Welcome to the Band Name Generator!\n')
+city = input("Name of the city you grew up in: ")
+pet = input("Pet name: ")
 
 
-
-	
+print(f'Your band name is: {city} {pet}.')
---
+++  python_pro_bootcamp_2021/day2/main.py
@@ -0,0 +1,10 @@
+"""
+
+    Challenge Day 1: Tip Calculator.
+    Details: This program will calculate what percentage each person must pay from the bill and tip.
+    Author: Yan Brasiliano SIlva Penalva <yanpenabr@gmail.com>
+    Date: 18 MAR 2021
+    Course:  Python Pro Bootcamp for 2021
+    Level: Beginner
+
+"""
