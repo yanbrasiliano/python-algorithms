d000ddcf1f334381ad421cc24271b84046d3f2ff
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2021-04-22 01:09:30 -0300

    Update paint_calculator.py

---
+++ python_pro_bootcamp/day8/paint_calculator.py
@@ -0,0 +1,11 @@
+def paint_calc(height,width,cover):
+	paint=(height*width)/cover
+	return paint
+
+
+
+height_wall=float(input('Height of wall: '))
+width_wall=float(input('Width of wall: '))
+coverage=float(input('Coverage of wall: '))
+
+print(f"You'll need {round(paint_calc(height=height_wall,width=width_wall,cover=coverage))} cans of paint.")
