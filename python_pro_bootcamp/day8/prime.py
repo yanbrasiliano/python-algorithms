ba1cbe1f46954e58365964e05ca7127cba0f1f76
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2021-04-24 02:41:53 -0300

    Create prime.py

---
+++ python_pro_bootcamp/day8/prime.py
@@ -0,0 +1,15 @@
+def prime_checker(n):
+	is_prime=True
+	for i in range(2, n):
+		if n % i == 0:
+			is_prime=False
+	if is_prime:
+		print("It's a prime number.")
+	else:
+		print("It's not a prime number.")
+
+
+
+
+number = int(input('Check this number: '))
+prime_checker(number)
