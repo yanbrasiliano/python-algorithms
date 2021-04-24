6f36e853a1f562cd21cefd8b8b66d3eed478082e
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2021-04-24 19:48:13 -0300

    Update main.py

--- python_pro_bootcamp/day8/main.py
+++ python_pro_bootcamp/day8/main.py
@@ -25,6 +25,17 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
+def encrypt(text,shift):
+	cipher=""
+	for letter in alphabet:
+		pos=alphabet.index(letter)
+		newpos=pos+shift
+		newletter=alphabet[newpos]
+			
+
+
+
 direction = input("Type 'encode' to encrypt or type 'decode' to decrypt: ")
 text = input("Message: ").lower()
 shift = int(input("Shift number: "))
