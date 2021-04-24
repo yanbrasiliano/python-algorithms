b754f31f673dfd1165439d446b811b621471e649
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2021-04-24 11:48:23 -0300

    Update main.py

--- python_pro_bootcamp/day8/main.py
+++ python_pro_bootcamp/day8/main.py
@@ -19,6 +19,12 @@ logo = ['''
 
 
 ''']
-
-
 print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 
+'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+ 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt or type 'decode' to decrypt: ")
+text = input("Message: ").lower()
+shift = int(input("Shift number: "))
