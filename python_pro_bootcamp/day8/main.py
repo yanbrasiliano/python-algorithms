958a94e375202254672eaa1988d096fee77f8de0
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2021-05-19 00:12:37 -0300

    Update main.py

--- python_pro_bootcamp/day8/main.py
+++ python_pro_bootcamp/day8/main.py
@@ -8,41 +8,24 @@
 	
 """
 
-logo = [''' 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',' ']
 
-  ___   _   ___ ___   _   ___    ___ ___ ___ _  _ ___ ___ 
- / __| /_\ | __/ __| /_\ | _ \  / __|_ _| _ \ || | __| _ \
-| (__ / _ \| _|\__ \/ _ \|   / | (__ | ||  _/ __ | _||   /
- \___/_/ \_\___|___/_/ \_\_|_\  \___|___|_| |_||_|___|_|_\
+direct=input('Encode to encrypt, decode to decrypt: ').lower()
+text=input("Message: ").lower()
+shift=int(input("Shift number: "))
 
-
-''']
-print(logo)
-
-
-def caesar(text,shift,direction):
-	# deal with the case when the shift number is higher than the number of letters
-  # e.g. with 26 letters, the result of a shift by 27 is the same as a shift by 1
-	if shift > len(alphabet):
-		shift = shift % len(alphabet)
-	# in case of decoding
-if cipher_direction == "decode":
-  shift_amount = len(alphabet) - shift_amount
-cipher=" " 
-for letter in text:
-	
-	position=alphabet.index(letter) # posição das letras da frase/texto digitadas.
-	new_position=position+shift # a nova posição será a posição atual + os saltos solicitados.
-	new_letter=alphabet[new_position] # a nova letra baseada nos saltos.
-	cipher+=new_letter # a junção das letras.
-print(f'Encoded message: {cipher}')	# exposição do código cifrado.
+def caesar(text,shift,direct):
+	end=""
+	if direct == "decode":
+		shift*=-1
+	for letter in text:
+		position = alphabet.index(letter)
+		position+=shift
+		if position > 25: 
+			end = end + alphabet[position - 26]
+		else:
+			end = end + alphabet[position]
+	print(f"Here's the {direct} result: {end}.")
 
 
-
-
-
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-text = input("Message: ").lower()
-shift = int(input("Shift number: "))
-direction = input('Encode to encrypt, decode to decrypt: ')
-
+caesar(text,shift,direct)
