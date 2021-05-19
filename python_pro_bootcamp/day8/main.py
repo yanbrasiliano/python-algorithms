0e73e8b411286bbf093e5989c2c8810f7aef921f
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2021-05-18 21:54:04 -0300

    Update main.py

--- python_pro_bootcamp/day8/main.py
+++ python_pro_bootcamp/day8/main.py
@@ -19,18 +19,30 @@ logo = ['''
 ''']
 print(logo)
 
-def encrypt(text,shift):
-	cipher=" " # lista vazia para receber código cifrado.
-	for letter in text:
-		position=alphabet.index(letter) # posição das letras da frase/texto digitadas.
-		new_position=position+shift # a nova posição será a posição atual + os saltos solicitados.
-		new_letter=alphabet[new_position] # a nova letra baseada nos saltos.
-		cipher+=new_letter # a junção das letras.
-	print(f'Encoded message: {cipher}')	# exposição do código cifrado.
+
+def caesar(text,shift,direction):
+	# deal with the case when the shift number is higher than the number of letters
+  # e.g. with 26 letters, the result of a shift by 27 is the same as a shift by 1
+	if shift > len(alphabet):
+		shift = shift % len(alphabet)
+	# in case of decoding
+if cipher_direction == "decode":
+  shift_amount = len(alphabet) - shift_amount
+cipher=" " 
+for letter in text:
+	
+	position=alphabet.index(letter) # posição das letras da frase/texto digitadas.
+	new_position=position+shift # a nova posição será a posição atual + os saltos solicitados.
+	new_letter=alphabet[new_position] # a nova letra baseada nos saltos.
+	cipher+=new_letter # a junção das letras.
+print(f'Encoded message: {cipher}')	# exposição do código cifrado.
+
+
+
+
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 text = input("Message: ").lower()
 shift = int(input("Shift number: "))
-encrypt(text,shift)
-
+direction = input('Encode to encrypt, decode to decrypt: ')
 
